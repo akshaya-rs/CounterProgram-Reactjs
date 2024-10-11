79669b9f085037f10a8ddbfac182c6077f7cdb5b
akshaya-rs <77328432+akshaya-rs@users.noreply.github.com>
2024-10-11 09:18:26 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,35 @@
+import CounterProgram from './Components/CounterProgram.jsx';
+//import App from './Components/App';
+//import React from './react'
+
+function App() {
+  return(
+    <div>
+  <CounterProgram />
+  </div>
+);
+}
+/**
+  const contacts=[
+    {
+      id:"1",
+      "name":"Magesh",
+      "email": "max123@gmail.com",
+    },
+    {
+      id:"2",
+      "name":"Mugesh",
+      "email": "muz123@gmail.com",
+    }
+
+  ];
+  return (
+    <div>
+    <Header />
+    <AddContact />
+    <ContactList contacts={contacts}/>
+    </div>
+  );
+}**/
+
+export default App;
